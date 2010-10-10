d2bb3222a497334ae5dbd7217a2fddd5530aebfc
guilhiem <guilhiem@7774d95c-6e2f-e328-f6ab-95c32801c17b>
2010-10-10 15:30:09 +0000

    +UPDATED: Code cleanning following codding covention +Not need for "inline" Keyword when in .h (automatic) +Const order + No need for ; at the end of the line when inlining

--- Src/BurgerEngine/Graphics/SceneMesh.cpp
+++ Src/BurgerEngine/Graphics/SceneMesh.cpp
@@ -3,40 +3,41 @@
 #include "BurgerEngine/Graphics/StaticMesh.h"
 #include "BurgerEngine/Graphics/Material.h"
 
+//--------------------------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------------------------
 SceneMesh::SceneMesh( StaticMesh * pMesh )
 	: m_pMesh( pMesh )
 {
-	m_iPartCount = 0;
+	m_uPartCount = 0;
 }
 
-SceneMesh::~SceneMesh()
-{
-};
-
+//--------------------------------------------------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------------------------------------------------
 void SceneMesh::Draw( EffectTechnique::RenderingTechnique eTechnique )
 {
 
 	if( m_pMesh )
 	{
 		glPushMatrix();
-		glTranslatef( vPosition.x, vPosition.y, vPosition.z );
-		glRotatef( vRotation.x, 1, 0, 0 );	
-		glRotatef( vRotation.y, 0, 1, 0 );
-		glRotatef( vRotation.z, 0, 0, 1 );	
+		glTranslatef( m_f3Position.x, m_f3Position.y, m_f3Position.z );
+		glRotatef( m_f3Rotation.x, 1, 0, 0 );	
+		glRotatef( m_f3Rotation.y, 0, 1, 0 );
+		glRotatef( m_f3Rotation.z, 0, 0, 1 );	
 
-		glScalef( fScale, fScale, fScale);
+		glScalef( m_fScale, m_fScale, m_fScale);
 
-		for( unsigned int i = 0; i < m_iPartCount; ++i )
+		for( unsigned int i = 0; i < m_uPartCount; ++i )
 		{
-			if( m_oMaterials[ i ]->Activate( eTechnique ) )
+			if( m_vMaterials[ i ]->Activate( eTechnique ) )
 			{
 				m_pMesh->Render( i );
-				m_oMaterials[ i ]->Desactivate( eTechnique );
+				m_vMaterials[ i ]->Desactivate( eTechnique );
 			}
 		}
 
 		glPopMatrix();
-
 	}
 
 }
